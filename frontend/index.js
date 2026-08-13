357d5f4314a1e35fb860a1150054a28036da9346
karthik <shivamaniveeramalla@gmail.com>
2026-08-13 16:35:15 +0530

    toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,23 @@
-//alert("help");
+var useres=[
+    { 
+        "name":"John Doe",
+        "gender":"male",
+        "image":"john.png"
+    },
+    {
+        "name":"Jane Doe",
+        "gender":"female",
+        "image":"jane.png"
+    }
+]
+const btn=document.getElementById("btn");
+
+var curid=0;
+btn.addEventListener("click", toggleuser);
+function toggleuser(){
+        curid=(curid+1)%2;;
+        document.querySelector("img").src=useres[curid].image;
+        document.querySelector("h2").textContent=useres[curid].name;
+        document.querySelector("p").textContent=useres[curid].gender;
+
+}

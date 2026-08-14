1c1adc2ff8549e44f6104ba8706ec94c3d090137
karthik <shivamaniveeramalla@gmail.com>
2026-08-14 10:43:42 +0530

    randomuser

--- frontend/index.js
+++ frontend/index.js
@@ -11,13 +11,30 @@ var useres=[
     }
 ]
 const btn=document.getElementById("btn");
+const btn1=document.getElementById("btn1");
 
 var curid=0;
 btn.addEventListener("click", toggleuser);
 function toggleuser(){
-        curid=(curid+1)%2;;
-        document.querySelector("img").src=useres[curid].image;
-        document.querySelector("h2").textContent=useres[curid].name;
-        document.querySelector("p").textContent=useres[curid].gender;
+    curid=(curid+1)%2;;
+    document.querySelector("img").src=useres[curid].image;
+    document.querySelector("h2").textContent=useres[curid].name;
+    document.querySelector("p").textContent=useres[curid].gender;
 
+}
+btn1.addEventListener("click",randomuser);
+function randomuser(){
+    fetch("https://randomuser.me/api/")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        document.querySelector("img").src=data.results[0].picture.large;
+        document.querySelector("h2").textContent=data.results[0].name.first+""+data.results[0].name.last;
+        document.querySelector("p").textContent=data.results[0].gender;
+
+    })
+    .catch(function(err){
+        console.log("something error occured"+err);
+    });
 }
